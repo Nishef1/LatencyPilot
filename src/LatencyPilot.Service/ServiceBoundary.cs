40289dc735e9f477f72cc6c43e3f8e09b515d831
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-09-12 17:40:22 +0000

    feat(service): define observation-only authority

--- src/LatencyPilot.Service/ServiceBoundary.cs
+++ src/LatencyPilot.Service/ServiceBoundary.cs
@@ -2,5 +2,9 @@ namespace LatencyPilot.Service;
 
 public static class ServiceBoundary
 {
+    public const string ServiceName = "LatencyPilot Observation Service";
+
+    public const bool ObservationAvailable = true;
+
     public const bool MutationAvailable = false;
 }
