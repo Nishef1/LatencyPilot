4a1e34a43879027c9d79342b8826bf4880127fcd
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-09-13 09:08:01 +0000

    Add live CLEF log stream viewer

---
+++ scripts/Watch-LogStream.ps1
@@ -0,0 +1,103 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('APP', 'SERVICE')]
+    [string]$Component,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Directory,
+
+    [Parameter(Mandatory = $true)]
+    [string]$Pattern
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+function Get-EventValue {
+    param(
+        [Parameter(Mandatory = $true)]
+        [object]$Event,
+        [Parameter(Mandatory = $true)]
+        [string]$Name
+    )
+
+    $property = $Event.PSObject.Properties[$Name]
+    if ($null -eq $property) {
+        return $null
+    }
+
+    return $property.Value
+}
+
+function Write-LogLine {
+    param([Parameter(Mandatory = $true)][string]$Line)
+
+    try {
+        $event = $Line | ConvertFrom-Json
+        $timestampValue = Get-EventValue -Event $event -Name '@t'
+        $levelValue = Get-EventValue -Event $event -Name '@l'
+        $messageValue = Get-EventValue -Event $event -Name '@m'
+        $exceptionValue = Get-EventValue -Event $event -Name '@x'
+
+        $timestamp = if ($null -ne $timestampValue) {
+            ([DateTimeOffset]::Parse([string]$timestampValue)).ToLocalTime().ToString('HH:mm:ss.fff')
+        }
+        else {
+            '--:--:--.---'
+        }
+
+        $level = if ([string]::IsNullOrWhiteSpace([string]$levelValue)) {
+            'INF'
+        }
+        else {
+            ([string]$levelValue).ToUpperInvariant()
+        }
+
+        $message = if ([string]::IsNullOrWhiteSpace([string]$messageValue)) {
+            $Line
+        }
+        else {
+            [string]$messageValue
+        }
+
+        $color = switch ($level) {
+            'ERR' { 'Red' }
+            'FTL' { 'Red' }
+            'WRN' { 'Yellow' }
+            'DBG' { 'DarkGray' }
+            'VRB' { 'DarkGray' }
+            default { if ($Component -eq 'SERVICE') { 'Cyan' } else { 'Green' } }
+        }
+
+        Write-Host "[$timestamp][$Component][$level] $message" -ForegroundColor $color
+        if (-not [string]::IsNullOrWhiteSpace([string]$exceptionValue)) {
+            Write-Host ([string]$exceptionValue) -ForegroundColor Red
+        }
+    }
+    catch {
+        Write-Host "[$Component] $Line"
+    }
+}
+
+while (-not (Test-Path -LiteralPath $Directory -PathType Container)) {
+    Start-Sleep -Milliseconds 250
+}
+
+$file = $null
+while ($null -eq $file) {
+    $file = Get-ChildItem -LiteralPath $Directory -Filter $Pattern -File -ErrorAction SilentlyContinue |
+        Sort-Object LastWriteTimeUtc -Descending |
+        Select-Object -First 1
+
+    if ($null -eq $file) {
+        Start-Sleep -Milliseconds 250
+    }
+}
+
+Write-Host "[$Component] following $($file.FullName)" -ForegroundColor DarkGray
+Get-Content -LiteralPath $file.FullName -Tail 0 -Wait | ForEach-Object {
+    if (-not [string]::IsNullOrWhiteSpace($_)) {
+        Write-LogLine -Line $_
+    }
+}
