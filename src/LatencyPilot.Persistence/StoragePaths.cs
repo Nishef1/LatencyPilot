053802f5c24e2eb19d4a4d91f6ce1be9d5a5784f
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-09-13 11:06:22 +0000

    cleanup(persistence): remove unused storage helper

--- src/LatencyPilot.Persistence/StoragePaths.cs
+++
@@ -1,9 +0,0 @@
-namespace LatencyPilot.Persistence;
-
-public static class StoragePaths
-{
-    public static string GetApplicationDataRoot() =>
-        Path.Combine(
-            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
-            "LatencyPilot");
-}
