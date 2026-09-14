fb1fd1fed3ba33ba9981d76ae62ab4f5d9f57ce1
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-09-14 09:08:48 +0000

    fix(windows): complete MMDevice COM coclass declaration

--- src/LatencyPilot.Platform.Windows/Interop/CoreAudio.cs
+++ src/LatencyPilot.Platform.Windows/Interop/CoreAudio.cs
@@ -121,4 +121,6 @@ internal interface IConnector
 
 [ComImport]
 [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
-internal class MMDeviceEnumeratorComObject;
+internal class MMDeviceEnumeratorComObject
+{
+}
