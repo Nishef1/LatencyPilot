dbfa6bfbaf0130b4c33134812a72beae69b5ad39
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-09-15 18:40:23 +0000

    test(network): fold continuity edge cases into RSS contract

--- tests/LatencyPilot.CriticalTests/NetworkRssTests.cs
+++ tests/LatencyPilot.CriticalTests/NetworkRssTests.cs
@@ -1,7 +1,9 @@
+using System.Net.NetworkInformation;
 using LatencyPilot.Benchmarking.Optimization;
 using LatencyPilot.Core.Devices;
 using LatencyPilot.Core.Observation;
 using LatencyPilot.Platform.Windows.Devices;
+using LatencyPilot.Platform.Windows.System;
 using LatencyPilot.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -217,5 +219,83 @@ public sealed class NetworkRssTests
                     DpcDurationsMicroseconds = [],
                     IsrDurationsMicroseconds = [],
                 }).Status);
+
+        var targetInterface = new NetworkInterfaceContinuitySnapshot(
+            "interface-1",
+            rss.InterfaceDescription!,
+            NetworkInterfaceType.Ethernet,
+            OperationalStatus.Up,
+            12,
+            12,
+            ["192.0.2.10"],
+            ["192.0.2.1"],
+            WifiConnectionContinuityStatus.NotApplicable,
+            null,
+            null);
+        var continuityBefore = new NetworkEnvironmentContinuitySnapshot(
+            adapter.InstanceId,
+            rss,
+            targetInterface,
+            [targetInterface.InterfaceId],
+            new SystemAwakeTimeSnapshot(1_000, 10_000_000));
+        var continuityAfter = continuityBefore with
+        {
+            AwakeTime = new SystemAwakeTimeSnapshot(21_000, 210_000_000),
+        };
+        var stableContinuity = NetworkEnvironmentContinuity.Evaluate(continuityBefore, continuityAfter);
+        Assert.IsTrue(stableContinuity.IsStable);
+        Assert.AreEqual(
+            NetworkOptimizationReadinessStatus.Ready,
+            NetworkOptimizationReadiness.EvaluateForExperiment(
+                rss,
+                adapter,
+                benchmark,
+                attribution,
+                stableContinuity).Status);
+
+        var vpnChurn = NetworkEnvironmentContinuity.Evaluate(
+            continuityBefore,
+            continuityAfter with { UpInterfaceIds = [targetInterface.InterfaceId, "vpn-interface"] });
+        Assert.IsFalse(vpnChurn.IsStable);
+        Assert.AreEqual(
+            NetworkOptimizationReadinessStatus.Inconclusive,
+            NetworkOptimizationReadiness.EvaluateForExperiment(
+                rss,
+                adapter,
+                benchmark,
+                attribution,
+                vpnChurn).Status);
+
+        Assert.IsFalse(NetworkEnvironmentContinuity.Evaluate(
+            continuityBefore,
+            continuityAfter with
+            {
+                Rss = rss with { IndirectionTable = ["0:2", "0:4", "0:6", "0:10"] },
+            }).IsStable);
+
+        var wifiTarget = targetInterface with
+        {
+            InterfaceType = NetworkInterfaceType.Wireless80211,
+            WifiStatus = WifiConnectionContinuityStatus.Available,
+            WifiSsid = "TestWifi",
+            WifiBssid = "00:11:22:33:44:55",
+        };
+        var wifiBefore = continuityBefore with { TargetInterface = wifiTarget };
+        Assert.IsFalse(NetworkEnvironmentContinuity.Evaluate(
+            wifiBefore,
+            continuityAfter with
+            {
+                TargetInterface = wifiTarget with { WifiBssid = "00:11:22:33:44:66" },
+            }).IsStable);
+        Assert.IsFalse(NetworkEnvironmentContinuity.Evaluate(
+            wifiBefore,
+            continuityAfter with
+            {
+                TargetInterface = wifiTarget with
+                {
+                    WifiStatus = WifiConnectionContinuityStatus.PermissionDenied,
+                    WifiBssid = null,
+                },
+            }).IsStable);
     }
 }
