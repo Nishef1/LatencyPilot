3585d4ed712dce89b42587893c61d9308239ff89
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-09-15 16:35:46 +0000

    test(gpu): require capture continuity

--- tests/LatencyPilot.CriticalTests/GpuRuntimePlacementContractTests.cs
+++ tests/LatencyPilot.CriticalTests/GpuRuntimePlacementContractTests.cs
@@ -1,5 +1,7 @@
 using LatencyPilot.Benchmarking.Optimization;
 using LatencyPilot.Platform.Windows.Devices;
+using LatencyPilot.Platform.Windows.System;
+using LatencyPilot.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LatencyPilot.CriticalTests;
@@ -8,7 +10,7 @@ namespace LatencyPilot.CriticalTests;
 public sealed class GpuRuntimePlacementContractTests
 {
     [TestMethod]
-    public void GateAPlacementRequiresResolvedIsrEvidenceConfinedToTargetProcessor()
+    public void GateAPlacementAndCaptureContinuityFailClosed()
     {
         var confirmed = new GpuInterruptRuntimePlacementEvidence(
             "PCI\\VEN_10DE&DEV_TEST",
@@ -87,5 +89,41 @@ public sealed class GpuRuntimePlacementContractTests
         Assert.IsFalse(GpuOptimizationSourceRevisionPolicy.IsExactMatch(expectedRevision, new string('b', 40)));
         Assert.IsFalse(GpuOptimizationSourceRevisionPolicy.IsExactMatch(expectedRevision, null));
         Assert.IsFalse(GpuOptimizationSourceRevisionPolicy.IsExactMatch("dirty", expectedRevision));
+
+        var balancedScheme = new Guid("381b4222-f694-41f0-9685-ff5bb260df2e");
+        var stablePower = new SystemPowerSnapshot(
+            SystemPowerLineState.Online,
+            BatteryPresent: false,
+            Charging: null,
+            BatteryPercent: null,
+            BatterySaverEnabled: false,
+            ActiveSchemeId: balancedScheme,
+            ActiveSchemeName: "Balanced",
+            UserConfiguredPowerModeId: Guid.Empty,
+            UserConfiguredPowerMode: LatencyPilot.Platform.Windows.System.UserConfiguredPowerMode.Balanced);
+        var stableContext = new GpuOptimizationCaptureContinuitySnapshot(
+            ProcessId: 42,
+            ProcessStartedAtUtc: DateTimeOffset.UnixEpoch,
+            ProcessName: "game",
+            ProcessSessionId: 1,
+            ActiveConsoleSessionId: 1,
+            Power: stablePower);
+
+        Assert.IsTrue(GpuOptimizationCaptureContinuity.Evaluate(stableContext, stableContext).IsStable);
+        Assert.IsFalse(GpuOptimizationCaptureContinuity.Evaluate(
+            stableContext,
+            stableContext with { ProcessStartedAtUtc = stableContext.ProcessStartedAtUtc.AddSeconds(1) }).IsStable);
+        Assert.IsFalse(GpuOptimizationCaptureContinuity.Evaluate(
+            stableContext,
+            stableContext with { ActiveConsoleSessionId = 2 }).IsStable);
+        Assert.IsFalse(GpuOptimizationCaptureContinuity.Evaluate(
+            stableContext with { ProcessSessionId = 2 },
+            stableContext with { ProcessSessionId = 2 }).IsStable);
+        Assert.IsFalse(GpuOptimizationCaptureContinuity.Evaluate(
+            stableContext,
+            stableContext with
+            {
+                Power = stablePower with { BatterySaverEnabled = true },
+            }).IsStable);
     }
 }
