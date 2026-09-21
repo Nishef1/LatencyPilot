09e12a618fe3b5021613cb8815314103c953d178
Nima Sharifkazemi <nima.nsk@hotmail.com>
2026-09-21 04:46:57 -0400

    refactor(benchmark): name Original scored ceiling explicitly

--- src/LatencyPilot.Benchmarking/Optimization/GpuOriginalBaselinePolicy.cs
+++ src/LatencyPilot.Benchmarking/Optimization/GpuOriginalBaselinePolicy.cs
@@ -4,7 +4,8 @@ public static class GpuOriginalBaselinePolicy
 {
     public const int PreferredRunCount = GpuRepeatabilityClusterSelector.RequiredRunCount;
     public const int ReplacementTriggerAttemptCount = GpuRepeatabilityClusterSelector.MaximumAttemptCount;
-    public const int MaximumPhysicalAttemptCount = GpuRepeatabilityClusterSelector.MaximumOriginalAttemptCount;
+    public const int MaximumScoredObservationCount = GpuRepeatabilityClusterSelector.MaximumOriginalAttemptCount;
+    public const int MaximumPhysicalAttemptCount = MaximumScoredObservationCount;
 
     public static bool HasRepeatableCluster(IEnumerable<double> onePercentLowFps)
     {
